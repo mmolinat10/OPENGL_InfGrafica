31e0930f85161931699ed6f921b7d053bb0411b5
Marc <marcmolina_10@hotmail.com>
2017-04-04 01:27:57 +0200

    Ejercicio texturas: codigo puesto y sin resultados visibles...pendiente de revision

--- Practica1/src/main.cpp
+++ Practica1/src/main.cpp
@@ -5,6 +5,7 @@
 #include <GLFW\glfw3.h>
 #include <iostream>
 #include "Shader.h"
+#include <SOIL.h>
 
 using namespace std;
 
@@ -14,13 +15,16 @@ const int floatsPerPosition = 3;
 bool WIREFRAME = false;
 const int numIndices = 6;
 const int sizeOfIndices = sizeof(int) * numIndices;
+GLuint texture;
+int widthImage = 512, heightImage = 512;
+
 // Positions of vertices on CPU
 GLfloat VertexBufferObject[] = {
-	//geometry				//colors
-	  0.5f,  0.5f, 0.0f,	0.0f, 0.6f, 0.5f,
-	  0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,
-	 -0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,
-	 -0.5f, 0.5f, 0.0f,		0.0f, 0.6f, 0.5f
+	//geometry				//colors			//texture coords
+	  0.5f,  0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	1.0f, 1.0f, //Top right
+	  0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	1.0f, 0.0f, //Bottom right
+	 -0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f,	0.0f, 0.0f, //Bottom left
+	 -0.5f, 0.5f, 0.0f,		1.0f, 1.0f, 0.0f,	0.0f, 1.0f	//Top left
 };
 
 // Indexes on CPU
@@ -92,11 +96,10 @@ int main() {
 	//que funcion se llama cuando se detecta una pulsaci�n de tecla en la ventana x
 	glfwSetKeyCallback(window, key_callback);
 
-	Shader shad = Shader("./src/SimpleVertexShader.vertexshader", "./src/SimpleFragmentShader.fragmentshader");
+	Shader shad = Shader("./src/textureVertex.vertexshader", "./src/textureFragment.fragmentshader");
 	GLuint vao, vbo, ebo;
 	
 	//cargamos los shader
-	//GLuint variableShader = glGetUniformLocation();
 
 	//practica wireframe
 	glGenVertexArrays(1, &vao); // Create new VAO
@@ -109,15 +112,36 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexBufferObject), VertexBufferObject, GL_STATIC_DRAW);
 	
 	// indicate how vertex attribute 0 should interpret data in connected VBO
-	glVertexAttribPointer(0, floatsPerPosition, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
+	glVertexAttribPointer(0, floatsPerPosition, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
 
 	// indicate that current VBO should be used with vertex attribute with index 0
 	glEnableVertexAttribArray(0);
 
-	//color
-	glVertexAttribPointer(1, floatsPerPosition, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
+	/*color
+	glVertexAttribPointer(1, floatsPerPosition, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);*/
 
+	//texture
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// Set the texture wrapping/filtering
+	float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+	// Load and generate the texture
+	unsigned char* image = SOIL_load_image("./src/texture.png", &widthImage, &heightImage, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImage, heightImage, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+	
+	
+	//end texture..
 
 	// Create new buffer that will be used to store indices
 	glGenBuffers(1, &ebo);
@@ -153,13 +177,16 @@ int main() {
 		glCullFace(GL_BACK);
 		////GL_CW sentido horario, GL_CCW sentido antihorario
 		glFrontFace(GL_CCW);
-
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
 		//establecer el shader
 		shad.USE();
 
 		//uniform variable
-		GLint variableShader = glGetUniformLocation(shad.Program, "offset");
-		glUniform1f(variableShader, abs(sin(glfwGetTime())) * 0.5f);
+		//GLint variableShader = glGetUniformLocation(shad.Program, "offset");
+		//glUniform1f(variableShader, abs(sin(glfwGetTime())) * 0.5f);
+		GLint variableShader = glGetUniformLocation(shad.Program, "ourTexture");
+		glUniform1i(variableShader, 0);
 
 		glBindVertexArray(vao);
 
@@ -171,7 +198,7 @@ int main() {
 		}
 
 		//pitar el VAO
-		glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, NULL);
+		glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, 0);
 
 		// reset bindings for VAO
 		glBindVertexArray(0);
@@ -182,6 +209,7 @@ int main() {
 		glfwPollEvents();
 	}
 	// // reset bindings for VAO, VBO and EBO and set free also with glDeleteVertexArrays()
+	
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
